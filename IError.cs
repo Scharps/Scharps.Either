e65804f8c659dd2d4a4566c77e9b43a6a38f49dc
Samuel James <507.s94j@gmail.com>
2022-09-30 22:08:02 +0100

    Initialise repo

---
+++ IError.cs
@@ -0,0 +1,7 @@
+namespace Scharps.Either
+{
+    public interface IError
+    {
+        public string Message { get; }
+    }
+}
---
+++ Result.cs
@@ -0,0 +1,61 @@
+﻿namespace Scharps.Either;
+
+public struct Result
+{
+    public static Result<TError> Ok<TError>()
+        where TError : IError
+    {
+        return new Result<TError>();
+    }
+
+    public static Result<TError> Error<TError>(TError error)
+        where TError : IError
+    {
+        return new Result<TError>(error);
+    }
+
+    public static Result<TError, TValue> Ok<TError, TValue>(TValue value)
+        where TError : IError
+    {
+        return new Result<TError, TValue>(value);
+    }
+
+    public static Result<TError, TValue> Error<TError, TValue>(TError error)
+    where TError : IError
+    {
+        return new Result<TError, TValue>(error);
+    }
+}
+
+public struct Result<TError>
+    where TError : IError
+{
+    public Result() { }
+    public Result(TError error)
+    {
+        Error = error;
+    }
+
+    public TError? Error { get; } = default;
+
+    public bool IsOk() => Error is null;
+}
+
+public struct Result<TError, TValue>
+    where TError : IError
+{
+    public Result(TValue value)
+    {
+        Value = value;
+    }
+
+    public Result(TError error)
+    {
+        Error = error;
+    }
+
+    public bool IsOk() => Error is null;
+
+    public TValue? Value { get; } = default;
+    public TError? Error { get; } = default;
+}
